19eb6d8c80d9556a83d144f8866c90017d6880c6
HazemMedhat <102475810+HazemMedhat@users.noreply.github.com>
2022-12-23 13:33:52 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,49 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <fstream>
-#define Fast ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-#define ll long long
-//(-1+sqrt(1+(8*n)))/2; to know if n is sum from 1 to k or not
 
-unordered_map<string,bool>m;  // to storage the dictionary
-string input;
-vector<vector<string>>ans;  // have all the possible sentences
-vector<string>v;
-void rec(int i,string take){
-    if(i==input.size()){
-        bool f=0;
-        if(take.empty()){
-            for(int k=0;k<v.size();k++){
-                if(!m[v[k]]){ f=0; break; } // the word is not a real word in english
-                else f=1;
+void print(string str)
+{
+    map<string, int> m;
+    string word = "";
+    for(int i =0 ; i < str.size();i++)
+    {
+        str[i] = tolower(str[i]);
+    }
+    auto it = remove_if(str.begin(), str.end(), ::ispunct);
+    str.erase(it, str.end());
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == ' ')
+        {
+            if (m.find(word) == m.end())
+            {
+                m.insert(make_pair(word, 1));
+                word = "";
+            }
+            else
+            {
+                m[word]++;
+                word = "";
             }
         }
-        if(f) ans.push_back(v);
-        return;
+        else
+        {
+            word += str[i];
+        }
     }
-    take+=input[i]; // add the char
-    rec(i+1,take);  // go to the next without adding to the vector
-    v.push_back(take);  // add to the vector
-    take="";
-    rec(i+1,take); // go to the next after adding to the vector
-    v.pop_back(); // remove from the vector
+    if (m.find(word) == m.end()){
+        m.insert(make_pair(word, 1));
+    }
+    else{
+        m[word]++;
+    }
+    for (auto& it : m) {
+        cout << it.first << " --> "<< it.second << endl;
+    }
 }
 
-int main(){
-    ifstream dic("dictionary.txt",ios::in) ;
-    string word;
-    while (!dic.eof()){ // read from the file
-        getline(dic,word);
-        m[word]=1;
+int main()
+{
+    fstream file;
+    string line;
+    cout<<"Enter the file name: ";
+    string n;
+    cin>>n;
+    file.open(n+".txt",ios::app);
+    cout<<"start writing your text and if you finished press 0: "<<endl;
+    string str;
+    while(file)
+    {
+        getline(cin,line);
+
+        if(line == "0")
+            break;
+
+        file << line << endl;
+        str = line;
     }
-    cin>>input;
-    for(int i=0;i<input.size();i++){  //tolower letters
-        if(input[i]<'a')
-            input[i]+=32;
-    }
-    rec(0,"");
-    for(auto i:ans){
-        for(auto j:i) cout<<j<<" ";
-        cout<<endl;
-    }
+    file.close();
+
+    print(str);
+    return 0;
 }
